f09704ef197987f44868a4cfe6d3a02c0caa8b98
JoacoMonzon <joaquinmonzon58@gmail.com>
2024-12-17 12:30:19 -0300

    Update detalle.js

--- detalle.js
+++ detalle.js
@@ -1,6 +1,5 @@
 const menus = [{nombre:"Inicio", url:"index.html"},
     {nombre:"Contacto", url:"contacto.html"},
-   
     ]
     
     function cargarmenu(){
@@ -14,53 +13,82 @@ const menus = [{nombre:"Inicio", url:"index.html"},
     }
     
     cargarmenu();
-    const productos = [
-        {nombre: "Heladera", precio: "1000", img: "Imagenes/h.jpg", id: "1", stock: 10},
-        {nombre: "Tele", precio: "6000", img: "Imagenes/t.jpg", id: "2", stock: 10},
-        {nombre: "Lavaropas", precio: "3000", img: "Imagenes/l.jpg", id: "3", stock: 10},
-        {nombre: "Microondas", precio: "4000", img: "Imagenes/m.jpg", id: "4", stock: 10},
-        {nombre: "Secadora", precio: "5000", img: "Imagenes/s.jpg", id: "5", stock: 10},
-        {nombre: "Gabinete", precio: "6000", img: "Imagenes/g.jpg", id: "6", stock: 10},
-        {nombre: "Monitor", precio: "7000", img: "Imagenes/mm.webp", id: "7", stock: 10},
-        {nombre: "Pc Gamer", precio: "8000", img: "Imagenes/p.jpg", id: "8", stock: 10}
-    ];
 
+let productodetalle = JSON.parse(localStorage.getItem("detalleproducto"));
 
-    function cargarproductos() {
-        let ventas = document.getElementById("boxproducto");
-        for (const producto of productos) {
-            let contenedor = document.createElement("div");
-            contenedor.innerHTML = `<div class="boxproducto">
-                <h3>${producto.nombre}</h3>
-                <img src="${producto.img}" alt="" width="100"> 
-                <p>$${producto.precio}</p>
-                <button onclick="verdetalle('${producto.id}')">detalle</button>
-                </div>`;
-            ventas.appendChild(contenedor);
-        }
-    }
-    
-    cargarproductos();
-    
-    function verdetalle(idproducto){
-        const buscarproducto=productos.find(producto => producto.id === (idproducto));
-        const enJSON =JSON.stringify(buscarproducto);
-        localStorage.setItem("detalleproducto", enJSON)
-        window,location.href= "detalle.html";
-    
-    }
-
-    function actualizarCarrito() {
-       
-        let cantidadTotal = parseInt(localStorage.getItem("cantidadCarrito"), 10) || 0;
-
+function cargarproducto() {
+    let enlaces = document.getElementById("boxproducto");
+    let lista = document.createElement("div");
+    lista.innerHTML = `<h3>${productodetalle.nombre}</h3>
+        <img src=${productodetalle.img} alt="" width="250">
+        <p>$ ${productodetalle.precio}</p>
+        <div class= "botonescontador">
+        <button onclick="sumar()">+</button>
+        <p id="contarproducto">0</p>
+        <button onclick="restar()">-</button>
+        </div>
+        <button onclick="cargarcarrito()" id="botoncargar">Agregar al carrito</button>`;
         
-        const cantidadCarritoElement = document.getElementById("cantidad-carrito");
-        if (cantidadCarritoElement) {
-            cantidadCarritoElement.textContent = cantidadTotal;
-        }
+        
+    enlaces.appendChild(lista);
+}
+cargarproducto();
+
+let contador = 0;
+
+function sumar() {
+    let nstock = productodetalle.stock;  // Asegúrate de que 'productodetalle' esté definido correctamente
+    if (contador < nstock) {
+        contador++;
+        document.getElementById("contarproducto").innerHTML = contador;
+    } else {
+        alert("Stock agotado!");
     }
+}
+
+
+function restar(){
+    if(contador > 0){
+    contador = contador -1;
+    document.getElementById("contarproducto").innerHTML = contador;
+}
+}
+
+function cargarcarrito(){
+    if (contador ===0) {
+        alert("Ingrese la cantidad de productos deseados")
+    }else {
+        let carrito = JSON.parse(localStorage.getItem("carrito"))
     
-    window.addEventListener('DOMContentLoaded', function() {
-        actualizarCarrito();
-    });
+    if (carrito === null) {
+        carrito = [];
+    }
+    cantidadproducto = parseInt( document.getElementById("contarproducto").innerHTML);
+    productonuevo = {id: productodetalle.id,
+        nombre: productodetalle.nombre,
+        cantidad: cantidadproducto,
+        precio: productodetalle.precio,
+        img: productodetalle.img
+    }
+    carrito.push(productonuevo)
+    const enJSON = JSON.stringify(carrito)
+    localStorage.setItem("carrito", enJSON)
+    window.location.href= "carrito.html";
+    }
+}
+
+function actualizarCarrito() {
+ 
+    let cantidadTotal = parseInt(localStorage.getItem("cantidadCarrito"), 10) || 0;
+
+    
+    const cantidadCarritoElement = document.getElementById("cantidad-carrito");
+    if (cantidadCarritoElement) {
+        cantidadCarritoElement.textContent = cantidadTotal;
+    }
+}
+
+
+window.addEventListener('DOMContentLoaded', function() {
+    actualizarCarrito();
+});
